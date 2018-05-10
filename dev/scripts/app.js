1c7aa5e4b951dc5b2db70d9bf1fe3e464031d45c
Quinn Anderson <quinnrhys89@gmail.com>
2018-05-09 22:59:55 -0400

    fine tuned the styling

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+const email = document.getElementById("email");
+const interestedIn = document.getElementById("interestedIn");
 
-class App extends React.Component {
-    render() {
-      return (
-        <div>
-          Hello
-        </div>
-      )
-    }
+const form = document.getElementById("form");
+
+const regEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+// const dotsAnimation =
+
+form.addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+  console.log(email.value);
+  console.log(interestedIn.value);
+
+  if (!regEx.test(email.value)) {
+    $('#error-message').hide().html("Please enter a valid e-mail address").fadeIn();
+  } else {
+    document.getElementById("error-message").innerHTML = " ";
+    document.getElementById("submit").value = "Submitting";
+    document.getElementById("submit").disabled = true;
+
+    setTimeout(function() {
+      form.style.display = "none";
+      document.getElementById("thankyou-msg").style.display = "inline";
+    }, 2000);
+  }
 }
-
-ReactDOM.render(<App />, document.getElementById('app'));
